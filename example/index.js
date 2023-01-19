26fa67f7c7956eccedef19c29801c873a08409c3
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-19 09:10:22 +0100

    Move example files into its own folder

---
+++ example/index.js
@@ -0,0 +1,7 @@
+import Slider from "../src/slider/slider.js"
+import image1 from './assets/ben-kim-r8P5yl7INzc-unsplash.jpg'
+import image2 from './assets/dan-arbely-fqREmUNzMxA-unsplash.jpg'
+
+const slider = new Slider([image1, image2]);
+
+document.body.appendChild(slider);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Slider from "./slider/slider"
-import image1 from './assets/ben-kim-r8P5yl7INzc-unsplash.jpg'
-
-const slider = new Slider([image1]);
-
-document.body.appendChild(slider);
